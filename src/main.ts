49de5d61049d9fc4ba5a722288d1551e52c5ab75
Jesse <jessey9527@gmail.com>
2018-05-17 21:38:42 +0800

    chore: add vuex, store, editor module

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -10,6 +11,12 @@ Vue.config.productionTip = false
 const app = new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
+
+// prevent contextmenu
+document.oncontextmenu = function (e: MouseEvent) {
+  e.preventDefault()
+}
---
+++ src/store/index.ts
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import editor from './modules/editor'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  modules: {
+    editor
+  },
+  state: {
+  },
+  mutations: {
+  }
+})
+
+export default store
